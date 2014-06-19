9784971adb0a2b6a5fef0c3b2dd6e8f88e21758d
xiangshouding <fansekey@gmail.com>
2014-06-19 18:22:18 +0800

    ...

--- test/app/app.js
+++
@@ -1,9 +0,0 @@
-var yog = require('../../');
-var app = require('express')();
-
-app.use(yog({
-    basedir: __dirname,
-    viewdir: __dirname + '/views'
-})).listen(4000, function () {
-    console.log('Listening *:4000');
-});
